27030650b6fa8cdb3a0c0cd8f120132de02f172d
Sherwin Variancia <sherwinvariancia@gmail.com>
2020-11-20 23:29:08 +0800

    New logo

--- resources/views/admin/layouts/partials/main.blade.php
+++ resources/views/admin/layouts/partials/main.blade.php
@@ -9,7 +9,10 @@
   @yield('meta-content')
 
   <link rel="icon" type='image/x-icon' href="{{ asset('images/brand/favicon.ico') }}">
-
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/brand/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/brand/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/brand/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('images/brand/site.webmanifest') }}">
   <!-- Fonts -->
   {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
