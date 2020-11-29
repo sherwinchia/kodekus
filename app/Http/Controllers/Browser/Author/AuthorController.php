ca169bdfc56d1e0d441f268610add4d5d0aa0321
Sherwin Variancia <sherwinvariancia@gmail.com>
2020-11-29 19:03:59 +0800

    Fixed author bug and metas

--- app/Http/Controllers/Browser/Author/AuthorController.php
+++ app/Http/Controllers/Browser/Author/AuthorController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Profile;
+use App\Models\Page;
+use SEOTools;
 
 class AuthorController extends Controller
 {
@@ -13,11 +15,25 @@ class AuthorController extends Controller
 
   public function show($slug)
   {
-    $author = Profile::where('slug', $slug)->first()->user;
-    return view(self::PATH . 'show', compact(['author']));
-    // if ($author->role == 'admin') {
-    //   return view(self::PATH . 'show', compact(['author']));
-    // }
-    // abort('404');
+    $author = Profile::where('slug', $slug)->first();
+    if ($author) {
+      $author = $author->user;
+
+    $metas = Page::where('name', 'Meta')->first();
+    $metas = unserialize($metas->content);
+
+    SEOTools::setTitle('Author &middot; ' . $author->full_name);
+    SEOTools::setDescription($metas[1]['content']);
+    SEOTools::setCanonical(route('browser.home.index'));
+
+    SEOTools::opengraph()->setUrl(route('browser.authors.show', $slug));
+    SEOTools::opengraph()->addProperty('type', 'articles');
+    
+    SEOTools::twitter()->setSite($metas[2]['content']);
+    SEOTools::twitter()->setUrl(route('browser.authors.show', $slug));
+
+      return view(self::PATH . 'show', compact(['author']));
+    }
+    abort('404');
   }
 }
