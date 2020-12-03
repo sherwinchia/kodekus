dc49d419b29f5797e5c8890da644baaca6ec629d
Sherwin Variancia <sherwinvariancia@gmail.com>
2020-12-03 08:29:38 +0800

    added spatie backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:run --only-db')
+                  ->weeklyOn(6, '01:00');
     }
 
     /**
